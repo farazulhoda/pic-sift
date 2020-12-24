8167436125f6aa5308636be87450b988c94474d5
farazul <firaz5973616@gmail.com>
2020-12-25 01:50:08 +0530

    viewController modified

--- PiCSiFT/PiCSiFT/ViewController.swift
+++ PiCSiFT/PiCSiFT/ViewController.swift
@@ -12,10 +12,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let image = UIImage(named: "sample")!
         
+        let rgbaImage = RGBAImage(image: image)!
+        
+        let avgRed = 107
+        
+        for y in 0..<rgbaImage.height {
+            for x in 0..<rgbaImage.width {
+                let index = y   *   rgbaImage.width + x
+                
+                var pixel = rgbaImage.pixels[index]
+                
+                let redDelta = Int(pixel.red) - avgRed
+               
+                var modifier = 1 +  4 * (Double(y) / Double(rgbaImage.height))
+                if (Int(pixel.red) < avgRed) {
+                    modifier = 1
+                }
+                
+                pixel.red = UInt8(max(min(225,Int(round(Double(avgRed) + modifier * Double(redDelta)))), 0))
+                rgbaImage.pixels[index] = pixel
+            }
+        }
+        let result = rgbaImage.toUIImage()
         
     }
-
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
+
 
